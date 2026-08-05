7dca31ad68bebe5f53c8881ebce8b0e506a45ccc
Mehulsuthar817 <154885299+Mehulsuthar817@users.noreply.github.com>
2026-08-05 23:04:31 +0530

    feat:create and test APIerror utill...

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
+import ApiError from "./utils/ApiError.js";
 
 const app = express();
 
@@ -13,11 +14,25 @@ app.use(express.json());
 
 app.use("/api/v1",indexRoutes);
 
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
     message: "Code Visualizer Backend API is running ",
   });
+});
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong"
+  const success = err.success !== undefined?err.success :false;
+
+
+  res.status(statusCode).json({
+    success:success,
+    statusCode:statusCode,
+    message:message
+  })
 });
 
 export default app;
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
 import ApiResponse from "../utils/ApiResponse.js";
+import ApiError from "../utils/ApiError.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.status(200).json(new ApiResponse(200, "welcome to code visualization"));
+});
+
+
+// testing the APIErorr working flow (ALL OK!)
+router.get("/user", (req, res, next) => {
+  try {
+    const d = false;
+    if (!d) {
+      throw new ApiError(401, "please login");
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
