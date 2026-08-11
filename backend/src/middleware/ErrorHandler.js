3c5364dca170636954395c3475376eafd7ad8cac
Mehulsuthar817 <154885299+Mehulsuthar817@users.noreply.github.com>
2026-08-12 00:45:59 +0530

    feat: enhance error handling

--- backend/src/middleware/ErrorHandler.js
+++ backend/src/middleware/ErrorHandler.js
@@ -1,14 +1,20 @@
 import ApiError from "../utils/ApiError.js";
 
 export const errorHandler = ((err,req,res,next)=>{
+  if (err.code === 11000) {
+    return res.status(409).json({
+      success: false,
+      statusCode: 409,
+      message: "An account with this email already exists.",
+    });
+  }
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong"
   const success = err.success !== undefined?err.success :false;
 
-
   res.status(statusCode).json({
     success:success,
     statusCode:statusCode,
-    message:message
+    message:(statusCode<500)?message:"something went wrong"
   })
 });
