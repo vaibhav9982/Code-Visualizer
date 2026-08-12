f480ff95ae5ae9821b1188029d1a498fdfffd4fa
Mehulsuthar817 <154885299+Mehulsuthar817@users.noreply.github.com>
2026-08-12 21:33:35 +0530

    feat: create fetch my project and test with postman

--- backend/src/controllers/project.controller.js
+++ backend/src/controllers/project.controller.js
@@ -3,17 +3,32 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 
+export const createProject = asyncHandler(async (req, res) => {
+  const { title, prompt, language, code } = req.body;
 
-export const createProject = asyncHandler(async (req,res)=>{
-    const  {title,prompt,language,code} = req.body;
+  if (!title || !prompt || !language || !code) {
+    throw new ApiError(400, "Please Enter all the details");
+  }
 
-    if(!title || !prompt || !language || !code){
-        throw new ApiError(400,"Please Enter all the details");
-    }
+  // AI workkkk but what to do fro response language and other part visualization the react code AI will generate and i have to show it in frontend side
+  const newProj = await Project.create({
+    title,
+    prompt,
+    language,
+    code,
+    userId: req.user.id,
+  });
 
-    // AI workkkk but what to do fro response language and other part visualization the react code AI will generate and i have to show it in frontend side
-    const newProj = await Project.create({title,prompt,language,code,userId:req.user.id});
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "New Project created Successfully", newProj));
+});
 
-    return res.status(201).json(new ApiResponse(201,"New Project created Successfully",newProj));
+export const projects = asyncHandler(async (req, res) => {
+  const projects = await Project.find({userId:req.user.id});
+  if(projects.length === 0){
+    return res.status(200).json(new ApiResponse(200,"You havent created any project",projects));
+  }
 
+  return res.status(200).json(new ApiResponse(200,"project fetched Successfully",projects));
 });
--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createProject } from "../controllers/project.controller.js";
+import { createProject, projects } from "../controllers/project.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/Projects",authMiddleware,createProject);
+router.get("/MyProjects",authMiddleware,projects);
 export default router;
